b2c689184b35d15d2616731696888551b7487927
keruipeng <keruipen@usc.edu>
2022-01-05 16:23:17 -0800

    updated analysis

--- master.extract_cdr3_IMREP.sh
+++ master.extract_cdr3_IMREP.sh
@@ -1,12 +1,12 @@
 # IMREP:
 # Parser for extracting cdr3 seq, count and frequency from IMREP output and converting to csv with sample name.
-ls raw_data/original/IMREP | cut -d "/" -f4 | awk -F ".cdr3" '{print $1}' > IMREP_samples.txt
+ls raw_data/75bp/IMREP | cut -d "/" -f4 | awk -F ".cdr3" '{print $1}' > IMREP_samples.txt
 
 while read line
 do
-	python scripts/imrep.extract.py raw_data/original/IMREP/${line}.cdr3 summary_data/original/IMREP/TRA_extracted_features/${line}.extracted_IMREP.csv TRA
+	python scripts/imrep.extract.py raw_data/75bp/IMREP/${line}.cdr3 summary_data/75bp/IMREP/TRB_extracted_features/${line}.extracted_IMREP.csv TRB
 
 done<IMREP_samples.txt
 
-echo "Sample,CDR3,nReads" > summary_data/original/IMREP_TRA_merged_extracted_features.csv
-tail -n +2 -q summary_data/original/IMREP/TRA_extracted_features/*.csv >> summary_data/original/IMREP_TRA_merged_extracted_features.csv
+echo "Sample,CDR3,nReads" > summary_data/75bp/IMREP_TRB_merged_extracted_features.csv
+tail -n +2 -q summary_data/75bp/IMREP/TRB_extracted_features/*.csv >> summary_data/75bp/IMREP_TRB_merged_extracted_features.csv
--- master.extract_cdr3_TRUST4.sh
+++ master.extract_cdr3_TRUST4.sh
@@ -1,16 +1,15 @@
 # TRUST:
 # Parser for extracting cdr3 seq, count and frequency from TRUST output and converting to csv with sample name.
-ls raw_data/original/TRUST4/ | cut -d "/" -f4 | awk -F ".tsv" '{print $1}' >TRUST_samples.txt
+ls raw_data/75bp/TRUST4/ | cut -d "/" -f4 | awk -F ".tsv" '{print $1}' >TRUST_samples.txt
 
 while read line
 do
-	python scripts/TRUST4.extract.py raw_data/original/TRUST4/${line}.tsv summary_data/original/TRUST4/TRA_extracted_features/${line}.extracted_TRUST4.csv TRA
+	python scripts/TRUST4.extract.py raw_data/75bp/TRUST4/${line}.tsv summary_data/75bp/TRUST4/TRB_extracted_features/${line}.extracted_TRUST4.csv TRB
 
 done<TRUST_samples.txt
 
-echo "Sample,CDR3,nReads" > summary_data/original/TRUST4_TRA_merged_extracted_features.csv
-tail -n +2 -q summary_data/original/TRUST4/TRA_extracted_features/*.csv >> summary_data/original/TRUST4_TRA_merged_extracted_features.csv
-
+echo "Sample,CDR3,nReads" > summary_data/75bp/TRUST4_TRB_merged_extracted_features.csv
+tail -n +2 -q summary_data/75bp/TRUST4/TRB_extracted_features/*.csv >> summary_data/75bp/TRUST4_TRB_merged_extracted_features.csv
 
 
 
