9659f7809ccc4df70be6794063075a463555ce88
keruipeng <keruipen@usc.edu>
2022-02-17 14:19:19 -0800

    update analysis

--- master.extract_cdr3_MIXCR.sh
+++ master.extract_cdr3_MIXCR.sh
@@ -1,13 +1,13 @@
 # MIXCR:
 # Parser for extracting cdr3 seq, count and frequency from MIXCR output and converting to csv with sample name.
 
-ls raw_data/50bp/MIXCR | cut -d "/" -f4 | awk -F ".txt" '{print $1}' > MIXCR_samples.txt
+ls raw_data/75bp/MIXCR | cut -d "/" -f4 | awk -F ".txt" '{print $1}' > MIXCR_samples.txt
 
 while read line
 do
-	python scripts/mixcr.extract.py raw_data/50bp/MIXCR/${line}.txt summary_data/50bp/MIXCR/TRB_extracted_features/${line}.extracted_MIXCR.csv TRB
+	python scripts/mixcr.extract.py raw_data/75bp/MIXCR/${line}.txt summary_data/75bp/MIXCR/TRB_extracted_features/${line}.extracted_MIXCR.csv TRB
 
 done<MIXCR_samples.txt
 
-echo "Sample,CDR3,nReads" > summary_data/50bp/MIXCR_TRB_merged_extracted_features.csv
-tail -n +2 -q summary_data/50bp/MIXCR/TRB_extracted_features/*.csv >> summary_data/50bp/MIXCR_TRB_merged_extracted_features.csv
+echo "Sample,CDR3,nReads" > summary_data/75bp/MIXCR_TRB_merged_extracted_features.csv
+tail -n +2 -q summary_data/75bp/MIXCR/TRB_extracted_features/*.csv >> summary_data/75bp/MIXCR_TRB_merged_extracted_features.csv
